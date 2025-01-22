67aa829b4a36ad09c75f042546526b824bf818ec
unknown <0x4rd1@gmail.com>
2025-01-22 15:56:18 +0700

    [NVIM] Add clear breakpoint keymap

--- nvim/lua/plugins/dap.lua
+++ nvim/lua/plugins/dap.lua
@@ -68,10 +68,66 @@ return {
     -- If you also want to debug C programs, add this:
     dap.configurations.c = dap.configurations.cpp
 
+    dap.adapters.go = function(callback, _)
+      local stdout = vim.loop.new_pipe(false)
+      local handle
+      local pid_or_err
+      local port = 38697
+      local opts = {
+        stdio = { nil, stdout },
+        args = { "dap", "-l", "127.0.0.1:" .. port },
+        detached = true,
+      }
+      handle, pid_or_err = vim.loop.spawn("dlv", opts, function(code)
+        stdout:close()
+        handle:close()
+        if code ~= 0 then
+          print("dlv exited with code", code)
+        end
+      end)
+      assert(handle, "Error running dlv: " .. tostring(pid_or_err))
+      stdout:read_start(function(err, chunk)
+        assert(not err, err)
+        if chunk then
+          vim.schedule(function()
+            require("dap.repl").append(chunk)
+          end)
+        end
+      end)
+      -- Wait for delve to start
+      vim.defer_fn(function()
+        callback({ type = "server", host = "127.0.0.1", port = port })
+      end, 100)
+    end
+    -- https://github.com/go-delve/delve/blob/master/Documentation/usage/dlv_dap.md
+    dap.configurations.go = {
+      {
+        type = "go",
+        name = "Debug",
+        request = "launch",
+        program = "${file}",
+      },
+      {
+        type = "go",
+        name = "Debug test", -- configuration for debugging test files
+        request = "launch",
+        mode = "test",
+        program = "${file}",
+      },
+      -- works with go.mod packages and sub packages
+      {
+        type = "go",
+        name = "Debug test (go.mod)",
+        request = "launch",
+        mode = "test",
+        program = "./${relativeFileDirname}",
+      },
+    }
+
     local keymap = vim.keymap.set
+
     local function focus_or_attach_dap(buffer_name, dap_ui_element)
       local found = false
-
       for _, win in ipairs(vim.api.nvim_list_wins()) do
         local buf = vim.api.nvim_win_get_buf(win)
         local buf_name = vim.api.nvim_buf_get_name(buf)
@@ -90,10 +146,12 @@ return {
     keymap("n", "<leader>ds", function()
       focus_or_attach_dap("DAP Scopes", "scopes")
     end, { desc = "Focus or open DAP Scopes" })
-
     keymap("n", "<leader>dr", function()
-      focus_or_attach_dap("dap%-repl%-", "repl") -- Focus or attach to DAP Repl
+      focus_or_attach_dap("dap%-repl%-", "repl")
     end, { desc = "Focus or Open DAP Repl" })
-    --
+    keymap("n", "<leader>dq", function()
+      dap.clear_breakpoints()
+      print("All breakpoints cleared")
+    end, { desc = "Clear all breakpoints" })
   end,
 }
