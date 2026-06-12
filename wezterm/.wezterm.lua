d1ff939c364eb1549f176c172a3b4915c818f075
Ardi Nugraha <0x4rd1@gmail.com>
2026-06-12 09:26:52 +0700

    add remember session wezterm

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,21 +1,14 @@
 local wezterm = require("wezterm")
 local bar = wezterm.plugin.require("https://github.com/adriankarlen/bar.wezterm")
-local config = {}
+local sessions = wezterm.plugin.require("https://github.com/abidibo/wezterm-sessions")
 
-bar.apply_to_config(config, {
-	modules = {
-		tabs = {
-			active_tab_fg = 1,
-			active_tab_bg = "#10B1FE",
-			inactive_tab_fg = "#94a3b8",
-		},
-		clock = {
-			enabled = false,
-		},
-	},
+local config = wezterm.config_builder()
+
+sessions.apply_to_config(config, {
+	auto_save_interval_s = 60,
+	git_branch_warn = true,
 })
 
--- **Basic Configuration**
 config.font = wezterm.font_with_fallback({
 	"JetBrainsMono NFM",
 	"Fira Code",
@@ -61,7 +54,6 @@ config.colors = {
 	},
 }
 
--- Hyperlink rules
 config.hyperlink_rules = {
 	{
 		regex = "\\((\\w+://\\S+)\\)",
@@ -94,7 +86,6 @@ config.hyperlink_rules = {
 	},
 }
 
--- Keybindings
 config.keys = {
 	{
 		key = "RightArrow",
@@ -111,13 +102,56 @@ config.keys = {
 		mods = "CTRL",
 		action = wezterm.action.CloseCurrentPane({ confirm = false }),
 	},
+	{
+		key = "w",
+		mods = "ALT",
+		action = wezterm.action.PromptInputLine({
+			description = wezterm.format({
+				{ Attribute = { Intensity = "Bold" } },
+				{ Foreground = { AnsiColor = "Fuchsia" } },
+				{ Text = "Enter name for workspace: " },
+			}),
+			action = wezterm.action_callback(function(window, pane, line)
+				if line then
+					window:perform_action(wezterm.action.SwitchToWorkspace({ name = line }), pane)
+				end
+			end),
+		}),
+	},
+	{
+		key = "l",
+		mods = "ALT",
+		action = wezterm.action.ShowLauncherArgs({ flags = "WORKSPACES" }),
+	},
+	{
+		key = "s",
+		mods = "ALT|SHIFT",
+		action = wezterm.action.EmitEvent("save_session"),
+	},
+	{
+		key = "r",
+		mods = "ALT|SHIFT",
+		action = wezterm.action.EmitEvent("load_session"),
+	},
 }
 
--- Conditional Default Program
 if wezterm.target_triple:find("windows") then
 	config.default_prog = { "powershell.exe" }
 elseif wezterm.target_triple:find("darwin") or wezterm.target_triple:find("linux") then
 	config.default_prog = { "zsh" }
 end
+
+bar.apply_to_config(config, {
+	modules = {
+		tabs = {
+			active_tab_fg = 1,
+			active_tab_bg = "#10B1FE",
+			inactive_tab_fg = "#94a3b8",
+		},
+		clock = {
+			enabled = false,
+		},
+	},
+})
 
 return config
